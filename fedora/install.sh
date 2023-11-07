99e748c1f16837de9302bdb55dd605b9dc759721
matteo-luraghi <matteoluraghi50@gmail.com>
2023-11-07 15:44:20 +0100

    added firefox backup

--- fedora/install.sh
+++ fedora/install.sh
@@ -15,6 +15,11 @@ dnf upgrade -y
 #Install basic packages
 dnf install gcc python3 btop firefox fzf tldr neofetch tree ca-certificates curl gnupg cowsay util-linux-user -y
 
+# Add firefox backup
+gpg $builddir/linux-installer/apps-settings/firefox.backup.tar.bz2.gpg
+tar -xf $builddir/linux-installer/apps-settings/firefox.backup.bz2
+rm -r $builddir/linux-installer/apps-settings/firefox.backup.bz2
+
 #Install and configure nvim
 dnf install neovim -y
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -14,10 +14,22 @@ apt upgrade -y
 
 apt install nala -y
 
+#Prepare firefox installation
 snap remove firefox
-
+sudo add-apt-repository ppa:mozillateam/ppa
+echo '
+Package: *
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1001
+' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+#Prevents updates to install snap firefox
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 #Install basic packages
 nala install build-essential vim python3 btop ffmpeg firefox fzf tldr neofetch tree ca-certificates curl gnupg cowsay -y
+# Add firefox backup
+gpg $builddir/linux-installer/apps-settings/firefox.backup.tar.bz2.gpg
+tar -xf $builddir/linux-installer/apps-settings/firefox.backup.bz2
+rm -r $builddir/linux-installer/apps-settings/firefox.backup.bz2
 
 #Install and configure nvim
 snap install nvim
