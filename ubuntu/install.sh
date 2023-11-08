5a3454c42e79db1b22709e14f545327c309f498a
matteo-luraghi <matteoluraghi50@gmail.com>
2023-11-08 09:33:25 +0100

    removed snap store

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -14,8 +14,31 @@ apt upgrade -y
 
 apt install nala -y
 
+#Remove snap
+snap remove --purge firefox
+snap remove --purge snap-store
+snap remove --purge gnome-3-38-2004
+snap remove --purge gnome-42-2204
+snap remove --purge gtk-common-themes
+snap remove --purge snapd-desktop-integration
+snap remove --purge bare
+snap remove --purge core20
+snap remove --purge canonical-livepatch
+snap remove --purge cups
+snap remove --purge core22
+snap remove core --revision 16091
+snap remove --purge snapd
+apt remove --autoremove snapd -y
+#File to not resintall snap
+echo 'Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+' | sudo tee /etc/apt/preferences.d/nosnap.pref
+nala update
+apt install --install-suggests gnome-software -y
+
 #Prepare firefox installation
-snap remove firefox
+cd $builddir
 sudo add-apt-repository ppa:mozillateam/ppa
 echo '
 Package: *
@@ -24,10 +47,12 @@ Pin-Priority: 1001
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 #Prevents updates to install snap firefox
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+
 #Install basic packages
-nala install build-essential vim python3 btop ffmpeg firefox fzf tldr neofetch tree ca-certificates curl gnupg cowsay -y
-# Add firefox backup
 cd $builddir
+nala install build-essential vim python3 btop ffmpeg firefox fzf tldr neofetch tree ca-certificates curl gnupg cowsay -y
+
+# Add firefox backup
 gpg $builddir/linux-installer/apps-settings/firefox.backup.tar.bz2.gpg
 tar -xf $builddir/linux-installer/apps-settings/firefox.backup.bz2
 rm -r $builddir/linux-installer/apps-settings/firefox.backup.bz2
