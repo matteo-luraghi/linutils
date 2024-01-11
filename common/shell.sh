c3b5e7c66c6d5dfe358542634ea6f0ef0d750f4a
matteo-luraghi <matteoluraghi50@gmail.com>
2024-01-11 18:38:23 +0100

    added time fix for dual boot and removed debian

---
+++ common/shell.sh
@@ -0,0 +1,9 @@
+builddir=$(pwd)
+git clone https://github.com/pixegami/terminal-profile.git
+./terminal-profile/install_powerline.sh
+cd $builddir
+./terminal-profile/install_terminal.sh
+cd $builddir
+./terminal-profile/install_profile.sh
+cd $builddir
+rm -r terminal-profile
---
+++ common/time.sh
@@ -0,0 +1,2 @@
+#Prevents time error when using dual boot with windows
+sudo timedatectl set-local-rtc 1 --adjust-system-clock
