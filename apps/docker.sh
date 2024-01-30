88ea4d816ea5b0c932e4ece97c8ba253683d586c
matteo-luraghi <matteoluraghi50@gmail.com>
2024-01-30 11:29:56 +0100

    added webeep installer and moved docker to apps

--- apps/docker.sh
+++ apps/docker.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
 #Install docker
 dockerVersion=docker-desktop-4.25.0-amd64.deb
 modprobe kvm
-sudo install -m 0755 -d /etc/apt/keyrings -y
+install -m 0755 -d /etc/apt/keyrings -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
---
+++ apps/webeep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+wget -O webeep.deb "https://github.com/toto04/webeep-sync/releases/latest/download/webeep-sync-debian.deb"
+nala install ./webeep.deb -y
+rm webeep.deb
