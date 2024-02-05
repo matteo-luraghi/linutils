5d46ba8b77d89f58dd8d648aee28a1740e118296
matteo-luraghi <matteoluraghi50@gmail.com>
2024-02-05 19:32:09 +0100

    changed shell installation

--- tools/shell.sh
+++ tools/shell.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
 #Install custom shell and color scheme
 builddir=$(pwd)
 pip3 install --user powerline-status
@@ -11,7 +17,13 @@ sudo cp -a fonts/. ~/.fonts/
 fc-cache -vf ~/.fonts/
 
 # Install ZSH
+cd ~
 sudo apt install -y git-core zsh curl
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Copy theme to ZSH folder
+cp ~/linux-utils/tools/matteleo.zsh-theme ~/.oh-my-zsh/themes/
+
+cp .zshrc ~/.zshrc
 
 chsh -s $(which zsh)
--- ubuntu-setup/coding.sh
+++ ubuntu-setup/coding.sh
@@ -9,7 +9,6 @@ goVersion=go1.21.3.linux-amd64.tar.gz
 wget https://go.dev/dl/$goVersion
 chmod u+x $goVersion
 tar -C /usr/local -xzf $goVersion
-echo 'export PATH=$PATH:/usr/local/go/bin' >> .zshrc
 rm $goVersion
 
 #Install Rust
