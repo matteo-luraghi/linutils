19457c30768a90a9553bfd8728c8dc3c16b3c381
matteo-luraghi <matteoluraghi50@gmail.com>
2024-08-24 09:25:13 +0200

    first attempts at ui

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use std::env::args;
+use std::io::{self, stdout};
 use std::process::Command;
 
-fn main() {
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm::{
+        event::{self, Event, KeyCode},
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+        ExecutableCommand,
+    },
+    layout::{Constraint, Layout},
+    widgets::{Block, Paragraph},
+    Frame, Terminal,
+};
+
+fn exec_command() {
     let distro = args().nth(1).expect("Error: no distro selected");
     println!("{}", distro);
 
@@ -13,4 +26,47 @@ fn main() {
         .output()
         .expect("failed to execute process");
     println!("{:?}", output)
+}
+
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    let _ = stdout().execute(EnterAlternateScreen);
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events()?;
+    }
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    return Ok(());
+}
+
+fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    return Ok(false);
+}
+
+fn ui(frame: &mut Frame) {
+    let [title_area, main_area, status_area] = Layout::vertical([
+        Constraint::Length(1),
+        Constraint::Min(0),
+        Constraint::Length(1),
+    ])
+    .areas(frame.area());
+    let [left_area, right_area] =
+        Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)])
+            .areas(main_area);
+
+    frame.render_widget(Block::bordered().title("Title Bar"), title_area);
+    frame.render_widget(Block::bordered().title("Status Bar"), status_area);
+    frame.render_widget(Paragraph::new("test").block(Block::bordered().title("Left")), left_area);
+    frame.render_widget(Block::bordered().title("Right"), right_area);
 }
