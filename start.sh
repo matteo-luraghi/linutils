7ce5551b8f4e5c795d03615cdfd0bd5e914a7ac3
matteo-luraghi <matteoluraghi50@gmail.com>
2024-08-25 17:24:23 +0200

    added start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Download Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Build the application
+cargo build --release
+
+# Run Linutils
+./target/release/linutils
