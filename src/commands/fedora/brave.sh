7d0263ff5cae43ac9fa1b98b094943e242ce200d
matteo-luraghi <matteoluraghi50@gmail.com>
2025-12-22 19:59:12 +0100

    fix: hyprland and brave scripts

--- src/commands/fedora/brave.sh
+++ src/commands/fedora/brave.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 sudo dnf install dnf-plugins-core
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 yes | sudo dnf install brave-browser
--- src/commands/fedora/hyprland.sh
+++ src/commands/fedora/hyprland.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+sudo dnf copr enable solopasha/hyprland -y
+
 # Install packages
-yes | sudo dnf install hyprland waybar dunst rofi-wayland wlogout xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprlock wl-clipboard cliphist mate-polkit
+yes | sudo dnf install hyprland waybar dunst rofi-wayland wlogout xdg-desktop-portal-hyprland qt5-qtwayland qt6-qtwayland hyprlock wl-clipboard cliphist mate-polkit
 
 # Copy the configurations
-cp -r /home/$USER/linutils/src/utils/dunst /home/$USER/.config/
-cp -r /home/$USER/linutils/src/utils/hypr /home/$USER/.config/
-cp -r /home/$USER/linutils/src/utils/ml4w /home/$USER/.config/
-cp -r /home/$USER/linutils/src/utils/rofi /home/$USER/.config/
-cp -r /home/$USER/linutils/src/utils/waybar /home/$USER/.config/
-cp -r /home/$USER/linutils/src/utils/wlogout /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/hyprland/dunst /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/hyprland/hypr /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/hyprland/ml4w /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/hyprland/rofi /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/hyprland/waybar /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/hyprland/wlogout /home/$USER/.config/
