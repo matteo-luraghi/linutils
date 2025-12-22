f9ed16ece009d8386769948df316aaf4c3c27e47
matteo-luraghi <matteoluraghi50@gmail.com>
2025-12-22 19:16:21 +0100

    feat(hypr): added script to install hyprland

--- src/commands/fedora/hyprland.sh
+++ src/commands/fedora/hyprland.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-yes | sudo dnf install hyprlock wl-clipboard cliphist mate-polkit
+# Install packages
+yes | sudo dnf install hyprland waybar dunst rofi-wayland wlogout xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprlock wl-clipboard cliphist mate-polkit
+
+# Copy the configurations
+cp -r /home/$USER/linutils/src/utils/dunst /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/hypr /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/ml4w /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/rofi /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/waybar /home/$USER/.config/
+cp -r /home/$USER/linutils/src/utils/wlogout /home/$USER/.config/
