986a52f534a1fbfd276a5f83b79552715e135a55
matteo-luraghi <matteoluraghi50@gmail.com>
2025-12-23 19:31:34 +0100

    feat(hyprland): use alacritty instead of kitty

--- src/utils/hyprland/hypr/scripts/workspaces.sh
+++ src/utils/hyprland/hypr/scripts/workspaces.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Workspace 1: Terminal
-hyprctl dispatch exec "[workspace 1 silent] kitty"
+hyprctl dispatch exec "[workspace 1 silent] alacritty"
 # Workspace 2: Browser
 hyprctl dispatch exec "[workspace 2 silent] brave-browser --ozone-platform-hint=auto --enable-features=WaylandWindowDecorations"
 # Workspace 3: Sessionized terminal with nvim, lazygit
-hyprctl dispatch exec "[workspace 3 silent] kitty -e ./code.sh"
+hyprctl dispatch exec "[workspace 3 silent] alacritty -e ./code.sh"
 
 notify-send "Ready to code"
