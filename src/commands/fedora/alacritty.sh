7bd55f4a06e29a0b89abf0acbe004c2b4df75185
matteo-luraghi <matteoluraghi50@gmail.com>
2025-12-24 16:44:03 +0100

    feat(alacritty): correct font for alacritty in fedora

--- src/commands/fedora/alacritty.sh
+++ src/commands/fedora/alacritty.sh
@@ -6,9 +6,5 @@ sudo dnf install alacritty -y
 # Setup alacritty
 mkdir -p /home/$USER/.config/alacritty
 cp /home/$USER/linutils/src/utils/alacritty.toml /home/$USER/.config/alacritty
-
-# Open terminal with ctrl+alt+tab
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'alacritty'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'alacritty'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>T'
+cp /home/$USER/linutils/src/utils/fonts/RobotoMono /home/$USER/.local/share/fonts
+fc-cache -fv
