cc1b8e6f7b94e49bb8accb6ce4b6d9608a25bc9e
matteo-luraghi <matteoluraghi50@gmail.com>
2026-01-07 19:40:03 +0100

    fix(bluetooth audio): added pavucontrol and blueman-manager

--- src/commands/fedora/hyprland.sh
+++ src/commands/fedora/hyprland.sh
@@ -3,7 +3,7 @@
 sudo dnf copr enable solopasha/hyprland -y
 
 # Install packages
-yes | sudo dnf install hyprland waybar dunst rofi-wayland wlogout xdg-desktop-portal-hyprland qt5-qtwayland qt6-qtwayland hyprlock wl-clipboard cliphist mate-polkit
+yes | sudo dnf install hyprland waybar dunst rofi-wayland wlogout xdg-desktop-portal-hyprland qt5-qtwayland qt6-qtwayland hyprlock wl-clipboard cliphist mate-polkit blueman bluez pavucontrol
 
 # Copy the configurations
 cp -r /home/$USER/linutils/src/utils/hyprland/dunst /home/$USER/.config/
