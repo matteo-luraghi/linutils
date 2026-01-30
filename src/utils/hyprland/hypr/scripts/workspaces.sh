051fe8d5d5c6067ab74381e41580969fa5c640a1
matteo-luraghi <matteoluraghi50@gmail.com>
2026-01-30 18:16:05 +0100

    feat: updated coding command

--- src/utils/hyprland/hypr/scripts/workspaces.sh
+++ src/utils/hyprland/hypr/scripts/workspaces.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Workspace 1: Terminal
-hyprctl dispatch exec "[workspace 1 silent] alacritty"
-# Workspace 2: Browser
-hyprctl dispatch exec "[workspace 2 silent] flatpak run app.zen_browser.zen"
-# Workspace 3: Sessionized terminal with nvim, lazygit
-hyprctl dispatch exec "[workspace 3 silent] alacritty -e ./code.sh"
+# Workspace 1: Browser
+hyprctl dispatch exec "[workspace 1 silent] flatpak run app.zen_browser.zen"
+# Workspace 2: Sessionized terminal with nvim, lazygit
+hyprctl dispatch exec "[workspace 2 silent] alacritty -e ./code.sh"
 
 notify-send "Ready to code"
